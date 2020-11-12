e55b3b5e3140bc928a4c5e96dcfb4253caeed39f
Darius Petermann <darius@Dariuss-MacBook-Pro.local>
2020-11-11 17:11:44 -0700

    Updated schema link

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/darius522/demo-protocol/tree/master/VoicePilot/VoicePilot_schema',
+  githubSrc: 'https://raw.githubusercontent.com/darius522/demo-protocol/master/VoicePilot/VoicePilot_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
